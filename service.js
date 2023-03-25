00a3c41f1215cb2fa0ba1c8ddf677d943d23f3c0
Rafael Zardo <rafael.zardo@outlook.com>
2023-03-25 14:43:47 -0300

    validarEntradaDeDados melhorada

--- service.js
+++ service.js
@@ -1,12 +1,58 @@
-// ctrl t para dar imprimir dados
+function verificaElementosDoCPF(cpf) {
+   return /^\d+$/.test(cpf); 
+}
+
+// Essa função segue o algoritmo de validação de CPF descrito na página oficial da Receita Federal do Brasil
+function validaDigitosVerificadoresDoCPF(cpf) {
+   const cpfNumerico = cpf.replace(/\D/g, '');
+ 
+   if (cpfNumerico.length !== 11) {
+     return false;
+   }
+ 
+   const digitosIguais = /^(\d)\1+$/.test(cpfNumerico);
+
+   if (digitosIguais) return false;
+
+   // Valida o primeiro dígito verificador
+   let soma = 0;
+
+   for (let i = 0; i < 9; i++) {
+     soma += parseInt(cpfNumerico.charAt(i)) * (10 - i);
+   }
+ 
+   let digitoVerificador1 = 11 - (soma % 11);
+   if (digitoVerificador1 > 9) digitoVerificador1 = 0;
+ 
+   if (digitoVerificador1 !== parseInt(cpfNumerico.charAt(9))) return false;
+ 
+   // Valida o segundo dígito verificador
+   soma = 0;
+
+   for (let i = 0; i < 10; i++) {
+     soma += parseInt(cpfNumerico.charAt(i)) * (11 - i);
+   }
+ 
+   let digitoVerificador2 = 11 - (soma % 11);
+   if (digitoVerificador2 > 9) digitoVerificador2 = 0;
+   
+   if (digitoVerificador2 !== parseInt(cpfNumerico.charAt(10))) return false;
+ 
+   // Se chegou até aqui, o CPF é válido
+   return true;
+}
+
 const validarEntradaDeDados = (lancamento) => {
    let entradaValida = true;
    let mensagemValidacao = "O lançamento não foi efetuado pela(s) seguinte(s) divergência(s): \n";
 
-   // A expressão regular /^\d+$/ verifica se a string contém apenas dígitos.
-   if (!/^\d+$/.test(lancamento.cpf)) { 
+   if (!verificaElementosDoCPF(lancamento.cpf)) { 
       entradaValida = false;
       mensagemValidacao += "- CPF contém caracteres inválidos. \n";
+   } 
+   else if (!validaDigitosVerificadoresDoCPF(lancamento.cpf)) {
+      entradaValida = false;
+      mensagemValidacao += "- CPF contém digitos verificadores inválidos. \n";      
    }
 
    
